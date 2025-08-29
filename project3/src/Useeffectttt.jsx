208e9b0931e0c2cdd8abd771c7542c870101640c
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-08-29 15:57:02 +0530

    UseEffect HOok also done

--- project3/src/Useeffectttt.jsx
+++ project3/src/Useeffectttt.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect , useState} from 'react'
 
 function Useeffectttt() {
+ let [count, setCount]= useState(0);
+  useEffect(()=>{ 
+    setTimeout(()=>{
+      setCount(++count)
+    },1000)
+  })
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Counting seconds from {count}</h1>
+    </>
   )
 }
 
